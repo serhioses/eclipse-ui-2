90ad2ce1d3614b6391f5e96bff6f7afe3ddfafa5
serhioses <serega.mirosh@gmail.com>
2017-08-15 16:21:23 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Serhiy Miroshnik's eclipse-ui library
---
+++ webpack.config.js
@@ -0,0 +1,62 @@
+const webpack = require('webpack'),
+  UglifyJSPlugin = require('uglifyjs-webpack-plugin'),
+  path = require('path'),
+  isDevelopment = !process.env.NODE_ENV || process.env.NODE_ENV === 'development';
+
+var config = [{
+  entry: {
+    'eclipse-ui': path.resolve(__dirname, './eclipse-ui.js'),
+    'eclipse-ui.min': path.resolve(__dirname, './eclipse-ui.js')
+  },
+  externals: {
+    jquery: {
+      commonjs: 'jquery',
+      commonjs2: 'jquery',
+      amd: 'jquery',
+      umd: 'jquery',
+      root: 'jQuery'
+    },
+    eclipse: {
+      commonjs: 'eclipse',
+      commonjs2: 'eclipse',
+      amd: 'eclipse',
+      umd: 'eclipse',
+      root: 'eclipse'
+    }
+  },
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: '[name].js',
+    libraryTarget: 'umd',
+    umdNamedDefine: false
+  },
+  resolve: {
+    extensions: ['*', '.js']
+  },
+  plugins: [
+    new webpack.NoEmitOnErrorsPlugin(),
+    new UglifyJSPlugin({
+      compress: {
+        warnings: false,
+        unsafe: true
+      },
+      include: /\.min\.js$/
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['es2015']
+        }
+      }
+    ]
+  },
+  devtool: isDevelopment ? 'cheap-inline-module-source-map' : false,
+  watch: isDevelopment
+}];
+
+module.exports = config;
