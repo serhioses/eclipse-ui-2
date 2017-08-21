831606a0c99a4ef3b20cf15f51adf875cecd4054
serhioses <serega.mirosh@gmail.com>
2017-08-21 09:40:13 +0300

    tabs

--- script.js
+++ script.js
@@ -73,15 +73,57 @@ $(document).ready(() => {
   // Dropdown (END)
 
   // Tabs
-  var t = new eclipse.UI.ResponsiveTabs($('#tabs'), {
-    hideAjacentTabsMobile: false,
-    hideAjacentTabsDesktop: true,
-    toggleTabsDesktop: false,
-    toggleTabsMobile: true,
-    bindDesktopToMobile: true,
-    bindMobileToDesktop: true
-  });
-  t.init();
+  // var t = new eclipse.UI.ResponsiveTabs($('#tabs'), {
+  //   desktopEffect: 'fade',
+  //   desktopSpeed: 1000,
+  //   mobileSpeed: 2000,
+  //   hideAjacentTabsMobile: true,
+  //   hideAjacentTabsDesktop: true,
+  //   toggleTabsDesktop: true,
+  //   toggleTabsMobile: true,
+  //   bindDesktopToMobile: false,
+  //   bindMobileToDesktop: true
+  // });
+  // t.init();
   // Tabs (END)
+
+  // requestAnimationFrame
+
+  function animate(options) {
+    var start = performance.now();
+
+    requestAnimationFrame(function animate(time) {
+      var timeFraction, progress;
+
+      // timeFraction от 0 до 1
+      timeFraction = (time - start) / options.duration;
+      if (timeFraction > 1) {
+        timeFraction = 1;
+      } else if (timeFraction < 0) {
+        timeFraction = 0;
+      }
+
+      // текущее состояние анимации
+      progress = options.timing(timeFraction)
+
+      options.draw(progress);
+
+      if (timeFraction < 1) {
+        requestAnimationFrame(animate);
+      }
+    });
+  }
+
+  var square = document.getElementById('square');
+
+  animate({
+    duration: 1000,
+    timing: function(timeFraction) {
+      return timeFraction;
+    },
+    draw: function(progress) {
+      square.style.opacity = 1 - (progress * 1);
+    }
+  });
   
 });
